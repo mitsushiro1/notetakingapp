665a358bfa64cc6b9f92acbff85b3b1077d69393
Mitsushiro Hirano <mitsushiro1@gmail.com>
2023-02-17 16:16:13 -0500

    added code

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const express = require("express");
 const path = require("path");
  const fs = require("fs");
 const PORT = process.env.PORT || 3001;
-let db = require("./db/db.json");
+let db = [];
+
+fs.readFile('./db/db.json', (err, data) => {
+  if (err) throw err;
+  db = JSON.parse(data);
+});
+
 const app = express();
 
 app.use(express.json());
@@ -14,45 +20,61 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    console.log(db)
-    res.status(200).json(db)
+    
+    console.log(db);
+    res.status(200).json(db);
 }
 )
 
 app.post('/api/notes', (req, res) =>{
-    console.log(req.body)
+    console.log(req.body);
     const newNote = {
         id: uuidv4(),
         ...req.body
     }
-    db.push(newNote)
-    fs.writeFile("./db/db.json", JSON.stringify(db), (err)=> err ? console.log(err): res.status(200).json(db))
+    db.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(db), (err)=> err ? console.log(err): res.status(200).json(db));
 }
 )
 
-app.delete('/api/notes/:id', (req, res)=>{
-    console.log(req.params.id)
-    const newArray = db.filter(notes => notes.id !== req.params.id)
-    console.log(newArray)
-    fs.writeFile("./db/db.json", JSON.stringify(newArray), (err)=> err ? console.log(err): res.status(200).json(newArray))
-
-}
-)
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Internal server error' });
+        return;
+      }
+  
+      const dbData = JSON.parse(data);
+      const newArray = dbData.filter(notes => notes.id !== noteId);
+  
+      fs.writeFile('./db/db.json', JSON.stringify(newArray), (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: 'Internal server error' });
+          return;
+        }
+  
+        res.status(200).json(newArray);
+      });
+    });
+});
 //routes
 app.get("/", (req, res) => {
 
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
 app.get("/notes", (req, res) => {
 
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
 
 
 app.listen(PORT, () => {
-    console.log("server has started!")
+    console.log("server has started!");
 }
 )
 
